7df05f3d6ef914c8f33ce0689c72a78de0d19671
Matt Williamson <matthew.williamson@gov>
2022-06-16 15:53:20 +0000

    Fixes for test builds

--- src/forcing/NetCDFPerFeatureDataProvider.cpp
+++ src/forcing/NetCDFPerFeatureDataProvider.cpp
@@ -1,5 +1,9 @@
+#ifdef NETCDF_ACTIVE
+
 #include "NetCDFPerFeatureDataProvider.hpp"
 #include <mutex>
 
 std::mutex data_access::NetCDFPerFeatureDataProvider::shared_providers_mutex;
 std::map<std::string, std::shared_ptr<data_access::NetCDFPerFeatureDataProvider>> data_access::NetCDFPerFeatureDataProvider::shared_providers;
+
+#endif
